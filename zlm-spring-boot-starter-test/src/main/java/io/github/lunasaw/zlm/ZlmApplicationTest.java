466a20c86d7ebeed0754cc5d8f8a6014fa168dee
chenzhangyue <chenzhangyue01@weidian.com>
2023-12-02 16:21:12 +0800

    :package: project init

---
+++ zlm-spring-boot-starter-test/src/main/java/io/github/lunasaw/zlm/ZlmApplicationTest.java
@@ -0,0 +1,18 @@
+package io.github.lunasaw.zlm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luna
+ * @version 1.0
+ * @date 2023/12/2
+ * @description:
+ */
+@SpringBootApplication
+public class ZlmApplicationTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZlmApplicationTest.class, args);
+    }
+}
